ea4e970008d357f9530e583f00b5800a0032d173
Seungyeon <52963399+jsyworks@users.noreply.github.com>
2025-01-04 18:31:55 +0900

    데이터베이스 개체(인덱스, 뷰, 프로시저) 생성 및 실행

---
+++ 챕터2/2-3 뷰 생성.sql
@@ -0,0 +1,5 @@
+create view member_view
+as
+select * From member;
+
+select * from member_view;
---
+++ 챕터2/2-3 인덱스 생성.sql
@@ -0,0 +1,3 @@
+select * from member where member_name = '아이유';
+
+create index idx_member_name on member(member_name);
